f1825eed3e9a3b396da3640610f9a94b272d5f43
RonsJose <g00464215@atu.ie>
2026-03-02 11:51:16 +0000

    Added controller and exceptions

---
+++ src/main/java/atu/ie/oop_exam_summer_50/Controller/BookController.java
@@ -0,0 +1,32 @@
+package atu.ie.oop_exam_summer_50.Controller;
+
+import atu.ie.oop_exam_summer_50.Model.Book;
+import atu.ie.oop_exam_summer_50.Service.BookService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class BookController {
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Book> createBook(@Valid @RequestBody Book book) {
+        bookService.addRoom(book);
+        return new ResponseEntity<>(book, HttpStatus.OK);
+    }
+
+    @GetMapping("/Show")
+    public ResponseEntity<List<Book>> showBook(){
+
+      return new ResponseEntity<>(bookService.getBookings(), HttpStatus.OK);
+    }
+}
